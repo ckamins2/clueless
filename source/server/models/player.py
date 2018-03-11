fce808e378457c05b252911232580961b79fcd9e
Patrick McQuay <patrick.mcquay@gmail.com>
2018-03-11 18:32:16 -0400

    Added player class basic initialization

--- source/server/models/player.py
+++ source/server/models/player.py
@@ -2,3 +2,13 @@
 class Player(object):
     name = ""
     character = None
+
+    def __init__(self, name, character):
+        self.name = name
+        self.character = character
+
+    def __init__(self, name):
+        self.name = name
+
+    def set_character(self, character):
+        self.character = character
