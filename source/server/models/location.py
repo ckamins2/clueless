b0cde6ed7ee24b44eb65d0942344b7eec6a61ade
Patrick McQuay <patrick.mcquay@gmail.com>
2018-03-11 18:31:50 -0400

    Updated Location class and tests to confirm room exclusion based on character count is correct

--- source/server/models/location.py
+++ source/server/models/location.py
@@ -3,13 +3,17 @@ from abc import ABCMeta, abstractmethod
 class Location():
     __metaclass__ = ABCMeta
     name = ""
-    adjacent_locations = {'N': None, 'E': None, 'S': None, 'W': None}
-    characters = []
+    adjacent_locations = None
+    characters = None
     max_characters = 0
 
     @abstractmethod
-    def __init__(self, name, adjacent_locations = []):
+    def __init__(self, name):
         self.name = name
+        if self.characters is None:
+            self.characters = []
+        if self.adjacent_locations is None:
+            self.adjacent_locations = {}
 
     def __str__(self):
         return self.name
@@ -34,18 +38,47 @@ class Location():
             if(location.is_valid_move_target()):
                 directions.append((location, direction))
 
-    def move_character_into_location(self, character):
+    def add_character(self, character):
         self.characters.append(character)
+
+    def remove_character(self, character):
+        self.characters.remove(character)
+
+    def is_character_in_room(self, character):
+        return character in self.characters
+
+    def north(self):
+        return self.adjacent_locations.get('N', None)
+
+    def south(self):
+        return self.adjacent_locations.get('S', None)
+
+    def east(self):
+        return self.adjacent_locations.get('E', None)
+
+    def west(self):
+        return self.adjacent_locations.get('W', None)
+
+    def secret(self):
+        return self.adjacent_locations.get('secret', None)
 
 
 class Room(Location):
 
-    def __init__(self, name, adjacent_locations = []):
-        super(Room, self).__init__(name, adjacent_locations)
+    def __init__(self, name):
+        super(Room, self).__init__(name)
         self.max_characters = 6
 
 class Hallway(Location):
 
-    def __init__(self, name, adjacent_locations = []):
-        super(Hallway, self).__init__(name, adjacent_locations)
+    def __init__(self, name,):
+        super(Hallway, self).__init__(name)
         self.max_characters = 1
+
+class StartRoom(Location):
+
+    def __init__(self):
+        super(StartRoom, self).__init__("")
+        # Setting this to zero because no one should ever
+        # move into this room
+        self.max_characters = 0
